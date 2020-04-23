7566c36ca3f2b14f1afdf3610c5ca2bb20d9c6a3
tamago324 <tamago_pad@yahoo.co.jp>
2020-04-23 19:50:04 +0900

    self -> manager

--- autoload/leaderf/python/commands/basic.py
+++ autoload/leaderf/python/commands/basic.py
@@ -6,98 +6,98 @@ from leaderf.utils import lfCmd, lfEval
 
 
 @_help.help("move the cursor upward")
-def command__down(self):
+def command__down(manager):
     lfCmd("normal! j")
-    self._previewResult(False)
+    manager._previewResult(False)
 
 
 @_help.help("move the cursor downward")
-def command__up(self):
+def command__up(manager):
     lfCmd("normal! k")
-    self._previewResult(False)
+    manager._previewResult(False)
 
 
-# def _page_up(self):
+# def _page_up(manager):
 #     lfCmd('normal! <PageUp>')
-#     self._previewResult(False)
+#     manager._previewResult(False)
 
-# def _page_down(self):
+# def _page_down(manager):
 #     lfCmd('normal! <PageDown>')
-#     self._previewResult(False)
+#     manager._previewResult(False)
 
-# def _left_mouse(self):
+# def _left_mouse(manager):
 #     lfCmd('normal! <LeftMouse>')
-#     self._previewResult(False)
+#     manager._previewResult(False)
 
 
 @_help.help("preview the result")
-def command__preview(self):
-    self._previewResult(True)
+def command__preview(manager):
+    manager._previewResult(True)
 
 
 @_help.help("toggle this help")
-def command__toggle_help(self):
-    self.toggleHelp()
+def command__toggle_help(manager):
+    manager.toggleHelp()
 
 
 @_help.help("quit")
-def command__quit(self):
-    self.quit()
+def command__quit(manager):
+    manager.quit()
 
 
 @_help.help("switch to input mode")
-def command__switch_insert_mode(self):
-    self.input()
+def command__switch_insert_mode(manager):
+    manager.input()
 
 
 @_help.help("open the file under cursor")
-def command__accept(self):
-    self.accept()
+def command__accept(manager):
+    manager.accept()
 
 
 @_help.help("open the file under cursor in horizontal split window")
-def command__accept_horizontal(self):
-    self.accept("h")
+def command__accept_horizontal(manager):
+    manager.accept("h")
 
 
 @_help.help("open the file under cursor in vertical split window")
-def command__accept_vertical(self):
-    self.accept("v")
+def command__accept_vertical(manager):
+    manager.accept("v")
 
 
 @_help.help("open the file under cursor new tabpage")
-def command__accept_tab(self):
-    self.accept("t")
+def command__accept_tab(manager):
+    manager.accept("t")
 
 
 @_help.help("scroll up in the popup preview window (nvim only)")
-def command__page_up_in_preview(self):
+def command__page_up_in_preview(manager):
     if lfEval("has('nvim')"):
-        self._toUpInPopup()
+        manager._toUpInPopup()
 
 
 @_help.help("scroll down in the popup preview window (nvim only)")
-def command__page_down_in_preview(self):
+def command__page_down_in_preview(manager):
     if lfEval("has('nvim')"):
-        self._toDownInPopup()
+        manager._toDownInPopup()
 
 
 @_help.help("close popup preview window (nvim only)")
-def command__close_preview_popup(self):
+def command__close_preview_popup(manager):
     if lfEval("has('nvim')"):
-        self._closePreviewPopup()
+        manager._closePreviewPopup()
 
 
 @_help.help("select multiple result")
-def command__add_selections(self):
-    self.addSelections()
+def command__add_selections(manager):
+    manager.addSelections()
 
 
 @_help.help("select all result")
-def command__select_all(self):
-    self.selectAll()
+def command__select_all(manager):
+    manager.selectAll()
 
 
 @_help.help("clear all selections")
-def command__clear_selections(self):
-    self.clearSelections()
+def command__clear_selections(manager):
+    manager.clearSelections()
--- autoload/leaderf/python/commands/ext.py
+++ autoload/leaderf/python/commands/ext.py
@@ -9,31 +9,31 @@ from utils import cd, nearestAncestor
 
 
 @_help.help("toggle show hidden files")
-def command__toggle_hidden_files(self):
-    self._getExplorer()._show_hidden_files = not self._getExplorer()._show_hidden_files
-    self.refresh(normal_mode=False)
+def command__toggle_hidden_files(manager):
+    manager._getExplorer()._show_hidden_files = not manager._getExplorer()._show_hidden_files
+    manager.refresh(normal_mode=False)
 
 
 @_help.help("show files of directory where g:Lf_RootMarkers exists")
-def command__goto_root_marker_dir(self):
+def command__goto_root_marker_dir(manager):
     root_markers = lfEval("g:Lf_RootMarkers")
-    rootMarkersDir = nearestAncestor(root_markers, self._getExplorer().cwd)
+    rootMarkersDir = nearestAncestor(root_markers, manager._getExplorer().cwd)
     if rootMarkersDir:
         # exists root_markers
-        self._chcwd(os.path.abspath(rootMarkersDir))
+        manager._chcwd(os.path.abspath(rootMarkersDir))
 
 
 @_help.help("change the current directory to cwd of LeaderF-filer")
-def command__change_directory(self):
-    cd(self._getExplorer().cwd)
-    lfCmd("echon ' cd {}'".format(self._getExplorer().cwd))
+def command__change_directory(manager):
+    cd(manager._getExplorer().cwd)
+    lfCmd("echon ' cd {}'".format(manager._getExplorer().cwd))
 
 
 @_help.help("go backwards in history")
-def command__history_backward(self):
-    self._chcwd(self._history.backward())
+def command__history_backward(manager):
+    manager._chcwd(manager._history.backward())
 
 
 @_help.help("go forwards in history")
-def command__history_forward(self):
-    self._chcwd(self._history.forward())
+def command__history_forward(manager):
+    manager._chcwd(manager._history.forward())
